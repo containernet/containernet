ba4a45e2d97959fc69deabdd5eacb7bc60c535c8
Manuel Peuster <manuel@peuster.de>
2015-12-17 14:05:11 +0100

    added empty test infrastructure for dockernet specific unit tests

---
+++ mininet/test/test_dockernet.py
@@ -0,0 +1,12 @@
+import unittest
+import os
+
+
+class testDockernet( unittest.TestCase ):
+
+    def testSingleDocker( self ):
+        print "Dockernet TEST"
+
+
+if __name__ == '__main__':
+    unittest.main()
