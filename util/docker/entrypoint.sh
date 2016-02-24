ca0fd47b90929542ee081883dbd80d180acbafa6
cgeoffroy <cgeoffroy@users.noreply.github.com>
2016-02-24 13:05:56 +0100

    Fix: moving the ubuntu image pull to the entrypoint script

--- util/docker/entrypoint.sh
+++ util/docker/entrypoint.sh
@@ -2,6 +2,13 @@
 
 service openvswitch-switch start
 
+if [ ! -S /var/run/docker.sock ]; then
+    echo 'Error: the Docker socket file "/var/run/docker.sock" was not found. It should be mounted as a volume.'
+    exit 1
+fi
+echo 'Pulling the "ubuntu:latest" image ... please wait'
+docker pull 'ubuntu:latest'
+
 if [[ $# -eq 0 ]]; then
     exec /bin/bash
 else
