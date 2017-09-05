1603f0051f6318c90a9e27b223ec13b8d50d423c
joka <joka@mail.uni-paderborn.de>
2017-09-05 18:34:32 +0200

    corrected image name

--- mininet/test/test_libvirt.py
+++ mininet/test/test_libvirt.py
@@ -29,7 +29,7 @@ class simpleTestTopology( unittest.TestCase ):
         self.l = []
         self.docker_cli = None
         self.lv_conn_qemu = libvirt.open('qemu:///system')
-        self.image_name = "/srv/images/test-vm1.qcow2"
+        self.image_name = "/srv/images/ubuntu16.04.qcow2"
         self.vm_names = ["vm1", "vm2", "vm3"]
         super(simpleTestTopology, self).__init__(*args, **kwargs)
 
