74c78b4d8c38e8e7fdda365a6943f22cb302f1b0
joka <joka@mail.uni-paderborn.de>
2017-11-14 22:13:15 +0100

    finally fix memory unit test

--- mininet/test/test_libvirt.py
+++ mininet/test/test_libvirt.py
@@ -413,11 +413,11 @@ class testContainernetContainerResourceLimitAPI( simpleTestTopology ):
         # check number of running docker containers
         # check connectivity by using ping: default link
         curmem = vm1.domain.memoryStats()['actual']
-        vm1.updateMemoryLimit(100000)
+        vm1.updateMemoryLimit(500)
         time.sleep(5)
         params = vm1.domain.memoryStats()
-        assert(long(params['actual']) == 100000)
-        vm1.updateMemoryLimit(curmem)
+        assert(long(params['actual']) == 500 * 1024)
+        vm1.updateMemoryLimit(curmem / 1024)
         # stop Mininet network
         self.stopNet()
 
