0969ec9655ef30623f997dc8b71be61dbdff76a8
Manuel Peuster <manuel.peuster@uni-paderborn.de>
2020-11-06 13:50:29 +0100

    Revert "Add storage_opt to Containernet"

--- mininet/node.py
+++ mininet/node.py
@@ -750,8 +750,7 @@ class Docker ( Host ):
                      'ipc_mode': None,
                      'devices': [],
                      'cap_add': [],
-                     'sysctls': {},
-                     'storage_opt': {},
+                     'sysctls': {}
                      }
         defaults.update( kwargs )
 
@@ -778,7 +777,6 @@ class Docker ( Host ):
         self.devices = defaults['devices']
         self.cap_add = defaults['cap_add']
         self.sysctls = defaults['sysctls']
-        self.storage_opt = defaults['storage_opt']
 
         # setup docker client
         # self.dcli = docker.APIClient(base_url='unix://var/run/docker.sock')
@@ -821,8 +819,7 @@ class Docker ( Host ):
             ipc_mode=self.ipc_mode,  # string
             devices=self.devices,  # see docker-py docu
             cap_add=self.cap_add,  # see docker-py docu
-            sysctls=self.sysctls,   # see docker-py docu
-            storage_opt=self.storage_opt
+            sysctls=self.sysctls   # see docker-py docu
             
         )
 
