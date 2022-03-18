2f3b542a1de10052e8250b83d0deb0597cad727b
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2022-03-18 17:06:01 -0300

    Add CentOS Stream 8 image

--- examples/example-containers/build.sh
+++ examples/example-containers/build.sh
@@ -6,4 +6,5 @@ docker build -t containernet_example:ubuntu1804 -f Dockerfile.ubuntu1804 .
 docker build -t containernet_example:ubuntu2004 -f Dockerfile.ubuntu2004 .
 
 docker build -t containernet_example:centos7 -f Dockerfile.centos7 .
+docker build -t containernet_example:centosstream8 -f Dockerfile.centosstream8 .
 docker build -t containernet_example:alpine3 -f Dockerfile.alpine3 .
