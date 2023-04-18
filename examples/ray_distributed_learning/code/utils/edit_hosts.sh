d9edc1e617b876f71ea552d75a220958fccdd3c4
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2023-04-18 15:54:50 +0200

    Refactor the training script and remove old files

--- examples/ray_distributed_learning/code/utils/edit_hosts.sh
+++ examples/ray_distributed_learning/code/utils/edit_hosts.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+# This script is used to deactivate the default network interface and configure the IP address
+# for the specified interface, so that Ray uses the correct interface for communication.
+
+# Get the hostname of the current machine
 HOST=$(hostname)
-sleep 0.5
+
+# Get the IP address of the specified interface (in this case, HOST-eth0)
 IP=$(ip -f inet addr show $HOST-eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+
+# Update the /etc/hosts file with the correct IP address and hostname
 ex -sc "%s/.*$HOST/$IP\t$HOST/g" -cx /etc/hosts
+
+# Deactivate the default eth0 interface
 ifconfig eth0 down
 
---
+++ examples/ray_distributed_learning/ray_training.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+"""
+Ray Workers in Containernet.
+"""
+import os
+from pathlib import Path
+import argparse
+
+from mininet.net import Containernet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import info, setLogLevel
+
+
+def create_net():
+    net = Containernet(controller=Controller)
+    info('*** Adding controller\n')
+    net.addController('c0')
+    return net
+
+
+def add_docker_containers(net, host_data_folder, host_results_folder, num_nodes):
+    info('*** Adding docker containers\n')
+    head_commands = (
+        f"/bin/bash -c 'python ./training_scripts/data/data.py --data all; "
+        f"/bin/bash ./utils/edit_hosts.sh;"
+        f"ray start --head --node-ip-address 10.0.0.100 --disable-usage-stats;"
+        f"exec /bin/bash'"
+    )
+    worker_commands = (
+        f"/bin/bash -c 'python ./training_scripts/data/data.py --data all;"
+        f"/bin/bash ./utils/edit_hosts.sh;"
+        f"ray start --address 10.0.0.100:6379 --disable-usage-stats;"
+        f"exec /bin/bash'"
+    )
+
+    workers_data_folder = '/root/data'
+    workers_results_folder = '/root/results'
+
+    head = net.addDocker('head', ip='10.0.0.100', dimage="ray:GPU", dcmd=head_commands, cpuset_cpus='0', cpus=1,
+                         shm_size="5000mb", dns=["8.8.8.8"], volumes=[f'{host_data_folder}:{workers_data_folder}',
+                                                                      f'{host_results_folder}:{workers_results_folder}'])
+    workers = []
+    for i in range(1, num_nodes):
+        workers.append(net.addDocker(f'worker_{i}', ip=f'10.0.0.{i + 100}', dimage="ray:GPU", dcmd=worker_commands,
+                                     cpuset_cpus=f'{i}', cpus=1, shm_size="5000mb", dns=["8.8.8.8"],
+                                     volumes=[f'{host_data_folder}:{workers_data_folder}',
+                                              f'{host_results_folder}:{workers_results_folder}']))
+
+    return head, workers
+
+
+def create_links(net, head, workers, delay):
+    info('*** Adding switches\n')
+    switch = net.addSwitch('s1')
+
+    info('*** Creating links\n')
+    net.addLink(head, switch, delay=f'{delay}ms')
+    for worker in workers:
+        net.addLink(worker, switch, delay=f'{delay}ms')
+
+
+def parse_args():
+    sudo_user = os.environ.get('SUDO_USER')
+    parser = argparse.ArgumentParser(description="Ray Workers in Containernet")
+    parser.add_argument('--data-dir', default=Path(f'/home/{sudo_user}') / 'data', help="Host directory for data folder")
+    parser.add_argument('--results-dir', default=Path(f'/home/{sudo_user}') / 'results', help="Host directory for results folder")
+    parser.add_argument('--num_nodes', type=int, default=4, help="Number of nodes (including the head node)")
+    parser.add_argument('--delay', type=float, default=0.004, help="Delay between nodes in milliseconds")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    host_data_folder = args.data_dir
+    host_results_folder = args.results_dir
+    num_nodes = args.num_nodes
+    delay = args.delay
+
+    setLogLevel('info')
+
+    net = create_net()
+    head, workers = add_docker_containers(net, host_data_folder, host_results_folder, num_nodes)
+    create_links(net, head, workers, delay)
+
+    info('*** Starting network\n')
+    net.start()
+    info('*** Testing connectivity\n')
+    net.ping([head, workers[0]])
+    info('*** Running CLI\n')
+    CLI(net)
+    info('*** Stopping network')
+    net.stop()
+
+
